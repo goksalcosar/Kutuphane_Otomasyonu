77e66137e2c50c56c6f7d22b01b2e5dd40be0d3d
Göksal Çoşar <98962680+GoksalCosar@users.noreply.github.com>
2022-06-22 19:38:26 +0300

    Update kutuhane_database_kiralanan_kitaplar.sql

--- KutuphaneOtomasyonu/Db/Ayrı Ayrı Tabloları/kutuhane_database_kiralanan_kitaplar.sql
+++ KutuphaneOtomasyonu/Db/Ayrı Ayrı Tabloları/kutuhane_database_kiralanan_kitaplar.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: kutuhane_database
+-- Host: 127.0.0.1    Database: kutuphane_database
 -- ------------------------------------------------------
 -- Server version	8.0.28
 
