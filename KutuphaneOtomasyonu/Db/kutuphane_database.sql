058c64f9af7a00d90d21ecc9ed00773315ea21b2
Göksal Çoşar <98962680+GoksalCosar@users.noreply.github.com>
2022-06-22 19:58:19 +0300

    Database Update

--- KutuphaneOtomasyonu/Db/kutuphane_database.sql
+++ KutuphaneOtomasyonu/Db/kutuphane_database.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: localhost:3306
--- Üretim Zamanı: 19 Haz 2022, 15:28:02
--- Sunucu sürümü: 8.0.28
+-- Üretim Zamanı: 22 Haz 2022, 16:57:32
+-- Sunucu sürümü: 5.7.33
 -- PHP Sürümü: 7.4.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -28,29 +28,29 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `kiralanan_kitaplar` (
-  `ID` int NOT NULL,
-  `ogrID` int NOT NULL,
+  `ID` int(11) NOT NULL,
+  `ogrID` int(11) NOT NULL,
   `ogrIsimSoyisim` varchar(50) CHARACTER SET utf8 COLLATE utf8_turkish_ci NOT NULL,
   `ogrYas` varchar(50) CHARACTER SET utf8 COLLATE utf8_turkish_ci NOT NULL,
   `ogrTc` varchar(50) CHARACTER SET utf8 COLLATE utf8_turkish_ci NOT NULL,
   `ogrAdres` varchar(80) CHARACTER SET utf8 COLLATE utf8_turkish_ci NOT NULL,
   `ogrEposta` varchar(50) CHARACTER SET utf8 COLLATE utf8_turkish_ci NOT NULL,
   `ogrTelefon` varchar(50) CHARACTER SET utf8 COLLATE utf8_turkish_ci NOT NULL,
-  `ktpID` int NOT NULL,
+  `ktpID` int(11) NOT NULL,
   `ktpBarkod` varchar(50) CHARACTER SET utf8 COLLATE utf8_turkish_ci NOT NULL,
   `ktpKitapAdi` varchar(50) CHARACTER SET utf8 COLLATE utf8_turkish_ci NOT NULL,
   `ktpYazar` varchar(50) CHARACTER SET utf8 COLLATE utf8_turkish_ci NOT NULL,
   `ktpYayinEvi` varchar(50) CHARACTER SET utf8 COLLATE utf8_turkish_ci NOT NULL,
-  `ktpSayfa` int NOT NULL,
+  `ktpSayfa` int(11) NOT NULL,
   `ktpTuru` varchar(50) CHARACTER SET utf8 COLLATE utf8_turkish_ci NOT NULL,
-  `RafNo` int NOT NULL,
+  `RafNo` int(11) NOT NULL,
   `Aciklama` varchar(150) CHARACTER SET utf8 COLLATE utf8_turkish_ci NOT NULL,
   `VerilisZamani` varchar(50) CHARACTER SET utf8 COLLATE utf8_turkish_ci DEFAULT NULL,
   `TeslimZamani` varchar(50) CHARACTER SET utf8 COLLATE utf8_turkish_ci DEFAULT NULL,
   `KitapDurumu` varchar(50) CHARACTER SET utf8 COLLATE utf8_turkish_ci DEFAULT 'Hasarsız',
   `TeslimDurumu` varchar(50) CHARACTER SET utf8 COLLATE utf8_turkish_ci DEFAULT 'Teslim Alınmadı',
   `TeslimAlimTarihi` varchar(50) CHARACTER SET utf8 COLLATE utf8_turkish_ci DEFAULT 'Teslim Alınmadı'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -59,17 +59,17 @@ CREATE TABLE `kiralanan_kitaplar` (
 --
 
 CREATE TABLE `kitaplar` (
-  `ID` int NOT NULL,
+  `ID` int(11) NOT NULL,
   `BarkodNo` varchar(50) CHARACTER SET utf8 COLLATE utf8_turkish_ci DEFAULT NULL,
   `KitapAdi` varchar(45) CHARACTER SET utf8 COLLATE utf8_turkish_ci DEFAULT NULL,
   `Yazar` varchar(45) CHARACTER SET utf8 COLLATE utf8_turkish_ci DEFAULT NULL,
   `YayinEvi` varchar(45) CHARACTER SET utf8 COLLATE utf8_turkish_ci DEFAULT NULL,
   `SayfaSayisi` varchar(45) CHARACTER SET utf8 COLLATE utf8_turkish_ci DEFAULT NULL,
   `Turu` varchar(45) CHARACTER SET utf8 COLLATE utf8_turkish_ci DEFAULT NULL,
-  `StokSayisi` int DEFAULT NULL,
-  `RafNo` int DEFAULT NULL,
+  `StokSayisi` int(11) DEFAULT NULL,
+  `RafNo` int(11) DEFAULT NULL,
   `Aciklama` varchar(250) CHARACTER SET utf8 COLLATE utf8_turkish_ci DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -78,10 +78,17 @@ CREATE TABLE `kitaplar` (
 --
 
 CREATE TABLE `loginpage` (
-  `ID` int NOT NULL,
-  `KullaniciAdi` varchar(45) CHARACTER SET utf8 COLLATE utf8_turkish_ci DEFAULT NULL,
-  `Sifre` varchar(45) CHARACTER SET utf8 COLLATE utf8_turkish_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_turkish_ci;
+  `ID` int(11) NOT NULL,
+  `KullaniciAdi` varchar(45) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `Sifre` varchar(45) COLLATE utf8_turkish_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Tablo döküm verisi `loginpage`
+--
+
+INSERT INTO `loginpage` (`ID`, `KullaniciAdi`, `Sifre`) VALUES
+(1, 'Göksal_Ç', '789');
 
 -- --------------------------------------------------------
 
@@ -90,14 +97,14 @@ CREATE TABLE `loginpage` (
 --
 
 CREATE TABLE `ogrenci` (
-  `ID` int NOT NULL,
+  `ID` int(11) NOT NULL,
   `IsimSoyisim` varchar(45) DEFAULT NULL,
-  `Yas` int DEFAULT NULL,
+  `Yas` int(11) DEFAULT NULL,
   `TcNo` varchar(45) DEFAULT NULL,
   `TelefonNo` varchar(45) DEFAULT NULL,
   `Adres` varchar(150) DEFAULT NULL,
   `Eposta` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dökümü yapılmış tablolar için indeksler
@@ -135,25 +142,25 @@ ALTER TABLE `ogrenci`
 -- Tablo için AUTO_INCREMENT değeri `kiralanan_kitaplar`
 --
 ALTER TABLE `kiralanan_kitaplar`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `kitaplar`
 --
 ALTER TABLE `kitaplar`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `loginpage`
 --
 ALTER TABLE `loginpage`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `ogrenci`
 --
 ALTER TABLE `ogrenci`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
